a4060d14169608b0eb7408957edc205bd24bde19
Maxime Roussin-Bélanger <lorack87@gmail.com>
2014-09-04 01:32:49 -0400

    Refactoring + added documentation

---
+++ src/main/java/com/lorack/astro_quizz/domain/MultipleChoiceQuestion.java
@@ -0,0 +1,14 @@
+package com.lorack.astro_quizz.domain;
+
+public class MultipleChoiceQuestion
+    extends Question
+{
+
+    public MultipleChoiceQuestion( String questionLabel, String[] choixReponse, int nbChoix, char reponse,
+                                   String picturePath )
+    {
+        super( questionLabel, choixReponse, nbChoix, reponse, picturePath );
+        // TODO Auto-generated constructor stub
+    }
+
+}
